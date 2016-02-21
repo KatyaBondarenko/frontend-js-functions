a88026fbab2a896b4f97f526dba3957159c42de6
KatyaBondarenko <superKatya25>
2016-02-21 22:16:31 +0200

    Function sum ()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-﻿function sum () {
+﻿function sum() {
 
-  return 378;
+  var sum = 0;
+
+  for (var i = 0; i < arguments.length; i++) {
+    if (!isNaN(arguments[i])){
+      if (!(typeof arguments[i] === 'string' || typeof arguments[i] === 'boolean')){
+        sum = sum + arguments[i];
+      }
+    }
+  }
+  return sum;
 }
+
+
+console.log(sum (null, false, "4", undefined, true, 99, 6));
