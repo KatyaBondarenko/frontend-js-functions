82838e3afa7c84ad3f734c5a3c2d389fe38a29d4
KatyaBondarenko <superKatya25>
2016-02-21 23:07:02 +0200

    function min

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 
   for (var i = 0; i < arguments.length; i++) {
     if (!isNaN(arguments[i])){
-      if (!(typeof arguments[i] === 'string' || typeof arguments[i] === 'boolean')){
+      if (!(typeof arguments[i] == 'string' || typeof arguments[i] == 'boolean')){
         sum = sum + arguments[i];
       }
     }
@@ -14,3 +14,40 @@
 
 
 console.log(sum (null, false, "4", undefined, true, 99, 6));
+
+
+
+
+function min(array) {
+
+
+  var min;
+  var isMin = false;
+
+  for (var i = 0; i < array.length; i++) {
+
+    if (!isNaN(array[i])){
+      if (!(typeof array[i] == 'string' || typeof array[i] == 'boolean' || typeof array[i] == 'object') ){
+        if (!isMin){
+          min = array[i];
+          isMin = true;
+        }
+        else{
+          if (array[i] < min)min = array[i];
+        }
+      }
+    }
+  }
+  return min;
+}
+
+var arr = [null, false, "4", undefined, true, 99, 524];
+
+
+console.log(min(arr));
+
+
+
+
+
+
